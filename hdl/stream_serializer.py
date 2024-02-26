c9df5e4e5673dc54c3a8580d1ffe76e4c4a0cb9d
occheung <ocheung@ucsb.edu>
2024-02-26 14:43:00 -0800

    serializer: widen

--- hdl/stream_serializer.py
+++ hdl/stream_serializer.py
@@ -6,8 +6,8 @@ import itertools
 
 class Serializer(Module):
     def __init__(self):
-        SRC_LAYOUT = [("data", 4)]
-        SINK_LAYOUT = [("data", 1)]
+        SRC_LAYOUT = [("data", 32)]
+        SINK_LAYOUT = [("data", 8)]
 
         self.submodules.source_fifo = stream.SyncFIFO(
             SRC_LAYOUT,     # layout
@@ -15,18 +15,26 @@ class Serializer(Module):
             # Keep buffer=False. It allows async reading
         )
 
-        self.submodules.sink_converter = stream.StrideConverter(
+        self.submodules.converter = stream.StrideConverter(
             SRC_LAYOUT,
             SINK_LAYOUT,
         )
 
-        self.comb += self.source_fifo.source.connect(self.sink_converter.sink)
+        self.submodules.sink_fifo = stream.SyncFIFO(
+            SINK_LAYOUT,
+            8,
+        )
+
+        self.comb += [
+            self.source_fifo.source.connect(self.converter.sink),
+            self.converter.source.connect(self.sink_fifo.sink),
+        ]
 
         self.io = set([ f[0] for f in itertools.chain(
-            self.source_fifo.sink.iter_flat(), self.sink_converter.source.iter_flat()) ])
+            self.source_fifo.sink.iter_flat(), self.sink_fifo.source.iter_flat()) ])
 
 
 if __name__ == "__main__":
     from migen.fhdl.verilog import convert
     module = Serializer()
-    convert(module, module.io).write("stream_producer.v")
+    convert(module, module.io).write("stream_serializer.v")
