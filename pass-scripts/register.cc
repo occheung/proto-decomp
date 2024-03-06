67e81fee029c794cd49eaed4a08498ad28e066ab
occheung <ocheung@ucsb.edu>
2024-03-06 13:15:44 -0800

    register: drop type comparison
    FFs with different static values is classified as different types, which weakens detection

--- pass-scripts/register.cc
+++ pass-scripts/register.cc
@@ -13,53 +13,75 @@ struct RegisterRecover : public Pass {
     RegisterRecover() : Pass("register") { }
 
     bool groupable(FfInitVals& ff_init_vals, const SigMap& sigmap, RTLIL::Cell* dff, RTLIL::Cell* dff_other) {
-        // Only DFFs of identical types can be grouped up together
-        if (dff->type != dff_other->type) {
-            return false;
-        }
-
+        // Only perform trivial matching
+        // So if the signal input polarity is different, do not group
         FfData dff_data(&ff_init_vals, dff);
         FfData dff_other_data(&ff_init_vals, dff_other);
-        if (dff_data.has_clk) {
-            if (sigmap(dff_data.sig_clk) != sigmap(dff_other_data.sig_clk)) {
+
+        if (dff_data.has_clk ^ dff_other_data.has_clk) {
+            return false;
+        } else if (dff_data.has_clk) {
+            if ((dff_data.pol_clk != dff_other_data.pol_clk) ||
+                    (sigmap(dff_data.sig_clk) != sigmap(dff_other_data.sig_clk))) {
                 return false;
             }
         }
 
-        if (dff_data.has_ce) {
-            if (sigmap(dff_data.sig_ce) != sigmap(dff_other_data.sig_ce)) {
+        if (dff_data.has_ce ^ dff_other_data.has_ce) {
+            return false;
+        } else if (dff_data.has_ce) {
+            if ((dff_data.pol_ce != dff_other_data.pol_ce) ||
+                    (sigmap(dff_data.sig_ce) != sigmap(dff_other_data.sig_ce))) {
                 return false;
             }
         }
 
-        if (dff_data.has_arst) {
-            if (sigmap(dff_data.sig_arst) != sigmap(dff_other_data.sig_arst)) {
+        if (dff_data.has_srst ^ dff_other_data.has_srst) {
+            return false;
+        } else if (dff_data.has_srst) {
+            if ((dff_data.pol_srst != dff_other_data.pol_srst) ||
+                    (sigmap(dff_data.sig_srst) != sigmap(dff_other_data.sig_srst))) {
                 return false;
             }
         }
 
-        if (dff_data.has_srst) {
-            if (sigmap(dff_data.sig_srst) != sigmap(dff_other_data.sig_srst)) {
+        // Both FF has the same has_srst and has_ce
+        // Check they respect the same CE/SRST precedence
+        if (dff_data.has_ce && dff_data.has_srst) {
+            if (dff_data.ce_over_srst != dff_other_data.ce_over_srst) {
                 return false;
             }
         }
 
-        if (dff_data.has_aload) {
-            if (sigmap(dff_data.sig_aload) != sigmap(dff_other_data.sig_aload)) {
+        if (dff_data.has_arst ^ dff_other_data.has_arst) {
+            return false;
+        } else if (dff_data.has_arst) {
+            if ((dff_data.pol_arst != dff_other_data.pol_arst) ||
+                    (sigmap(dff_data.sig_arst) != sigmap(dff_other_data.sig_arst))) {
                 return false;
             }
         }
 
-        if (dff_data.has_sr) {
-            if (sigmap(dff_data.sig_clr) != sigmap(dff_other_data.sig_clr)) {
-                return false;
-            }
-            if (sigmap(dff_data.sig_set) != sigmap(dff_other_data.sig_set)) {
+        if (dff_data.has_aload ^ dff_other_data.has_aload) {
+            return false;
+        } else if (dff_data.has_aload) {
+            if ((dff_data.pol_aload != dff_other_data.pol_aload) ||
+                    (sigmap(dff_data.sig_aload) != sigmap(dff_other_data.sig_aload))) {
                 return false;
             }
         }
 
-        // TODO: What if the FF is too primitive to have any of these features
+        if (dff_data.has_sr ^ dff_other_data.has_sr) {
+            return false;
+        } else if (dff_data.has_sr) {
+            if ((dff_data.pol_clr != dff_other_data.pol_clr) ||
+                    (dff_data.pol_set != dff_other_data.pol_set) ||
+                    (sigmap(dff_data.sig_clr) != sigmap(dff_other_data.sig_clr)) ||
+                    (sigmap(dff_data.sig_set) != sigmap(dff_other_data.sig_set))) {
+                return false;
+            }
+        }
+
         return true;
     }
 
@@ -69,20 +91,19 @@ struct RegisterRecover : public Pass {
 
         FfData ff_data(&ff_init_vals, ff);
 
-        std::ostringstream id_name;
-        id_name << ff_data.name.c_str() << "_consolidated";
-
         // Build the higher-level flip flops using the configs of the fine grain ff
         FfData fl_ff_data(ff_data);
         fl_ff_data.cell = nullptr;
-        fl_ff_data.name = id_name.str();
+        fl_ff_data.name = NEW_ID;
         // Set width & unselect fine gate
-        fl_ff_data.width = reg.size();
+        fl_ff_data.width = 0;
         fl_ff_data.is_fine = false;
         
         // Simply collect and reassign D pins and Q pins
         std::vector<RTLIL::SigBit> d_pins;
         std::vector<RTLIL::SigBit> q_pins;
+        std::vector<RTLIL::SigBit> clr_pins;
+        std::vector<RTLIL::SigBit> set_pins;
         std::vector<RTLIL::State> val_arsts;
         std::vector<RTLIL::State> val_srsts;
         std::vector<RTLIL::State> val_inits;
@@ -93,9 +114,16 @@ struct RegisterRecover : public Pass {
             const SigSpec& sig_q = single_ff_data.sig_q;
             d_pins.insert(d_pins.end(), sig_d.begin(), sig_d.end());
             q_pins.insert(q_pins.end(), sig_q.begin(), sig_q.end());
+
+            const SigSpec& sig_clr = single_ff_data.sig_clr;
+            const SigSpec& sig_set = single_ff_data.sig_set;
+            clr_pins.insert(clr_pins.end(), sig_clr.begin(), sig_clr.end());
+            set_pins.insert(set_pins.end(), sig_set.begin(), sig_set.end());
+
             val_arsts.insert(val_arsts.end(), single_ff_data.val_arst.bits.begin(), single_ff_data.val_arst.bits.end());
             val_srsts.insert(val_srsts.end(), single_ff_data.val_srst.bits.begin(), single_ff_data.val_srst.bits.end());
             val_inits.insert(val_inits.end(), single_ff_data.val_init.bits.begin(), single_ff_data.val_init.bits.end());
+            fl_ff_data.width += GetSize(sig_d);
         }
         fl_ff_data.sig_d = RTLIL::SigSpec(d_pins);
         fl_ff_data.sig_q = RTLIL::SigSpec(q_pins);
